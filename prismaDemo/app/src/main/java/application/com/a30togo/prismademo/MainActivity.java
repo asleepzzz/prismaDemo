01ac6e8501eec97485b78cf7386a12badf2a7422
Brad Chang <cutebrady@gmail.com>
2016-10-28 14:33:08 +0800

    Enhance user experience for preparing resources

--- prismaDemo/app/src/main/java/application/com/a30togo/prismademo/MainActivity.java
+++ prismaDemo/app/src/main/java/application/com/a30togo/prismademo/MainActivity.java
@@ -3,42 +3,38 @@ package application.com.a30togo.prismademo;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.RelativeLayout;
-import android.widget.Spinner;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private int QUERY_STYLE = 003;
 
+    Button mOnePic;
+    Button mMultiPic;
+    Toast mToast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mOnePic = (Button)findViewById(R.id.button_first);
+        mMultiPic = (Button)findViewById(R.id.multi_button);
+        mToast = Toast.makeText(this, "Preparing Resources ...", Toast.LENGTH_LONG);
+        mToast.show();
+
         Thread styleQueryThread = new Thread(new styleQueryHandler(""));
         styleQueryThread.start();
 
-        Button onePic = (Button)findViewById(R.id.button_first);
-        onePic.setOnClickListener(new View.OnClickListener() {
+        mOnePic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,CameraActivity.class);
@@ -47,8 +43,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button multiPic = (Button)findViewById(R.id.multi_button);
-        multiPic.setOnClickListener(new View.OnClickListener() {
+        mMultiPic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,CameraActivity.class);
@@ -108,6 +103,16 @@ public class MainActivity extends AppCompatActivity {
             String obj = (String) msg.obj;//
             if (result.equals("querysuccess")) {
                 Log.e("kevin","querysuccess");
+            }
+
+            if(mOnePic != null) {
+                mOnePic.setEnabled(true);
+            }
+            if(mMultiPic != null) {
+                mMultiPic.setEnabled(true);
+            }
+            if(mToast != null) {
+                mToast.cancel();
             }
         }
 
